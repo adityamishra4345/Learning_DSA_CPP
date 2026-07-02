3a9e6c167d6aab0d4d2e9369be8bf3a28fc12807
adityamishra4345 <adityamishra4345@gmail.com>
2026-07-02 22:04:37 +0530

    Power Of X Optimised

---
+++ Algorithms/Divide And Conquer/powerOfX.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+double power(double x,long long n){
+    if(n==0) return 1;
+    if(n<0){
+        x=1/x;
+        n=-n;
+    }
+    double half = power(x, n/2);
+    if(n%2==0) {
+        return (half*half);
+    }
+    else{
+        return (half*half*x);
+    }
+}
+int main(){
+    
+    cout << power(2, INT32_MIN) << "\n"; 
+    cout << power(2, -3) << "\n";   // 0.125
+    cout << power(3, 5)  << "\n";   // 243
+    
+
+    return 0;
+}
