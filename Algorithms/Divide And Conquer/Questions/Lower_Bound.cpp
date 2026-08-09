d26b25e2158d6cca9e91923553c44219c54f0275
Aditya Mishra <adityamishra4345@gmail.com>
2026-08-09 14:33:39 +0530

    Lower and upper bound done

---
+++ Algorithms/Divide And Conquer/Questions/Lower_Bound.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int lb(vector<int> arr,int left,int right,int target){
+    if(left>right) return arr.size();
+    
+    int mid=(left+right)/2;
+    if(arr[mid]>=target){
+        // int ans=mid;
+        int child=lb(arr,left,mid-1,target);
+        if(child==arr.size()){
+            return mid;
+        }
+        else{ 
+            return child;
+        }  
+    }
+    else{
+        return lb(arr,mid+1,right,target);
+    }
+}
+int main(){
+    vector<int> arr={1,2,3,3,3,4,5,5,6};
+    cout<<lb(arr,0,arr.size()-1,3);
+    return 0;
+}
